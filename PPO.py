1c1ff34111bc6112bfe220f34bb7721e383ff7b2
HendrikGrB <hendrik.grossebeckmann@udo.edu>
2023-08-03 15:42:40 +0200

    changed hyperparameters

--- PPO.py
+++ PPO.py
@@ -27,9 +27,9 @@ class PPO:
         self.critic_optim = torch.optim.Adam(self.critic.parameters(), lr=self.lr)
 
     def _init_hyperparameters(self):
-        self.rollout_steps = 100  # timesteps per episode
+        self.rollout_steps = 200  # timesteps per episode
         self.gamma = 0.99
-        self.n_updates_per_iteration = 4
+        self.n_updates_per_iteration = 50
         self.clip = 0.2
         self.lr = 0.00025
 
