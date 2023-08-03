881fe10d39afd7c8bfb798407d2e4c75e91caedf
HendrikGrB <hendrik.grossebeckmann@udo.edu>
2023-08-03 19:20:02 +0200

    changed layer init

--- Network.py
+++ Network.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
-    torch.nn.init.orthogonal_(layer.weight, std)
+    torch.nn.init.kaiming_normal_(layer.weight, std)
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
