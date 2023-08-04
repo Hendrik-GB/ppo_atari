b90a848902dcd282498b48c5db8359e1631ee1aa
HendrikGrB <hendrik.grossebeckmann@udo.edu>
2023-08-04 20:16:28 +0200

    changed network structure

--- Network.py
+++ Network.py
@@ -13,28 +13,22 @@ class CNN(nn.Module):
 
     def __init__(self, out_dims):
         super().__init__()
-        self.network = nn.Sequential(
-            layer_init(nn.Conv2d(1, 32, 8, stride=4)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(32, 64, 4, stride=2)),
-            nn.ReLU(),
-            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
-            nn.ReLU(),
-            nn.Flatten(),
-            layer_init(nn.Linear(64 * 7 * 7, 512)),
-            nn.ReLU(),
-        )
+        self.conv1 = layer_init(nn.Conv2d(in_channels=1, out_channels=32, kernel_size=8, stride=4))
+        self.conv2 = layer_init(nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2))
+        self.conv3 = layer_init(nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1))
+        self.lin = nn.Linear(in_features=7 * 7 * 64, out_features=512)
+        self.activation = nn.ReLU()
 
         self.actor = layer_init(nn.Linear(512, out_dims), std=0.01)
         self.critic = layer_init(nn.Linear(512, 1), std=1)
 
     def action_only(self, x):
-        x = self.network(x)
-        x = self.actor_activation(self.actor(x))
+        x = self.forward(x)
+        x = self.actor(x)
         return x
 
     def action_score(self, x):
-        x = self.network(x)
+        x = self.forward(x)
         action = self.actor(x)
         score = self.critic(x)
         return action, score.squeeze()
@@ -44,10 +38,10 @@ class CNN(nn.Module):
         x = x.unsqueeze(dim=-3)
         x = x / 255.0
         # print(torch.min(self.conv1.weight.data), torch.max(self.conv1.weight.data))
-        x = self.pool1(self.norm1(self.conv1(x)))
-        x = self.pool2(self.norm2(self.conv2(x)))
-        x = self.pool3(self.norm3(self.conv3(x)))
-        x = x.reshape((-1, 2 * 2 * 64))
+        x = self.activation(self.conv1(x))
+        x = self.activation(self.conv2(x))
+        x = self.activation(self.conv3(x))
+        x = x.reshape((-1, 7 * 7 * 64))
         x = self.activation(self.lin(x))
         return x
 
